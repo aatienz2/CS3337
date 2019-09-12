a274477dbe6c753dceedb13bae3afa8ed4eac23b
aatienz2 <55116176+aatienz2@users.noreply.github.com>
2019-09-11 19:08:54 -0700

    Create enroll.php

---
+++ enroll.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>New User Enroll</title>
+</head>
+<body>
+
+	<h1 align="center">
+		New User: Please submit your information
+	</h1>
+	
+	<form action="insertUser.php" method="post">
+    <table align="center">
+    <tr>
+        <td>
+        Email
+        </td>
+		<td>
+		<input type="text" name="email">
+		</td>
+    </tr>
+	<tr>
+        <td>
+        Password
+        </td>
+		<td>
+		<input type="text" name="password">
+		</td>
+    </tr>
+	<tr>
+		<td>
+		<input type="submit" value="submit">
+		</td>
+	</tr>
+    </table>
+	</form>
+
+</body>
+</html>

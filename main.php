3d67b323d62fbdb6bb3559bdc31776fcdcc672cd
aatienz2 <55116176+aatienz2@users.noreply.github.com>
2019-09-25 18:50:12 -0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main Page</title>
+	<link href="main.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+
+	<?php
+		include("main_menu.php");
+	?>
+	
+	<p align="center">
+		<img src="main_page.jpg" width=800/>
+	</p>
+</body>
+</html>
